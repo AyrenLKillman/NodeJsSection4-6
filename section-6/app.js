691b317c72ad4896838d306f4503511177591b33
Ayren Killman <ayrenkillman@gmail.com>
2025-01-16 13:35:08 -0600

    Vid 5 almost at end

--- section-6/app.js
+++ section-6/app.js
@@ -1,1 +1,9 @@
-//! from section 6
+const request = require('request')
+//! this is currently depreciated, if this weather app part does not function, that is probably why
+
+const url = "https://api.weatherstack.com/current?access_key=2c0a0045f17e83ac61011b100adf6e32&query=37.8267,-122.4233&units=f"
+
+request({url: url, json: true}, (error, response) => {
+    console.log("it is currently " + response.body.current.temperature + " degrees out. it feels like " +  response.body.current.feelslike + " degrees out")
+})
+//!vid 5
