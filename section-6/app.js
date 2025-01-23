139fa33f2cf9b347d3e845aa0894d8f7fe760da4
Ayren Killman <ayrenkillman@gmail.com>
2025-01-23 13:32:55 -0600

    added destructuring and property shorthands

--- section-6/app.js
+++ section-6/app.js
@@ -1,30 +1,24 @@
-const request = require('request')
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 //! bunch of this is outdated/changed, so sadly not everything works :(
-// const url = "https://api.weatherstack.com/current?access_key=2c0a0045f17e83ac61011b100adf6e32&query=37.8267,-122.4233&units=f"
 
+const address = process.argv[2]
 
-// request({url: url, json: true}, (error, response) => {
-//     if (error){
-//         console.log('unable to connect')
-//     } else if (response.body.error) {
-//         console.log('unable to find location')
-//     } 
-//     else {
-//    console.log(response.body.current.weather_descriptions[0] + ". it is currently " + response.body.current.temperature + " degrees out. it feels like " +  response.body.current.feelslike + " degrees out")
-// }
-// })
+if (!address) {
+    console.log('add a location pls')
+} else {geocode(address, (error, {latitude, longitude, location }) => {
+    if (error) {
+       return console.log(error)
+    }
+    forecast(latitude, longitude, (error, forecastdata) =>{
+        if (error) {
+            return console.log(error)
+         }
 
-
-
-
-geocode('Boston', (error, data) => {
-    console.log('error', error)
-    console.log('Data', data)
+         console.log(location)
+         console.log(forecastdata)
+    })
 })
 
-forecast(-75.7088, 44.1545, (error, data) =>{
-    console.log('error', error)
-    console.log('Data', data)
-})
+}
+
--- section-6/utils/forecast.js
+++ section-6/utils/forecast.js
@@ -4,16 +4,16 @@ const request = require('request')
 const forecast = (latitude, longitude,  callback) => {
     const url = "https://api.weatherstack.com/current?access_key=2c0a0045f17e83ac61011b100adf6e32&query=" + latitude +',' + longitude  + "&units=f"
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('unable to connect to services', undefined)
-        } else if (response.body.error){
+        } else if (body.error){
             callback("unable to find location", undefined)
         } else {
             callback(undefined, {
-                desc: response.body.current.weather_descriptions[0],
-                temp:  response.body.current.temperature,
-                feels: response.body.current.feelslike
+                desc: body.current.weather_descriptions[0],
+                temp:  body.current.temperature,
+                feels: body.current.feelslike
             })
 
         }
