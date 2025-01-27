c603ef0863c784c672031e88b38c97f5d70fadc3
Ayren Killman <ayrenkillman@gmail.com>
2025-01-27 14:09:45 -0600

    using handlebars

--- section-7-web/src/app.js
+++ section-7-web/src/app.js
@@ -1,14 +1,30 @@
 const path = require('path')
 const express = require('express')
 
+//!rewatch vid 7-7
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
-
 app.get('', (req, res) => {
-    res.send('sdf')
+    res.render('index', {
+        title: 'weather app'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'aboht mah'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('about', {
+        title: 'help i guess'
+    })
 })
 
 
